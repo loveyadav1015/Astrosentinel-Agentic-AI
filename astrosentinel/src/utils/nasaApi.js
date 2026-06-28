21995734e4271912fcb523156130a1f24a0d2786
loveyadav1015 <loveyadav2538@gmail.com>
2026-06-28 16:46:46 +0530

    fix: update nasaapi utility to use production environment variable

--- astrosentinel/src/utils/nasaApi.js
+++ astrosentinel/src/utils/nasaApi.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Point this to your new Express backend
-const BACKEND_URL = 'http://localhost:5000/api/neo';
+const BACKEND_URL = `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api/neo`;
 
 export const fetchNeoData = async () => {
   try {
